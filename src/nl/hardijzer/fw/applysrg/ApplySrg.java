0b5f26676ff2305d21a60f6107d1fe1712738471
Frans-Willem Hardijzer <fw@hardijzer.nl>
2012-04-28 01:13:42 +0200

    Added license

--- src/nl/hardijzer/fw/applysrg/ApplySrg.java
+++ src/nl/hardijzer/fw/applysrg/ApplySrg.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2012 Frans-Willem Hardijzer
+ * This file is part of SrgTools.
+ *
+ * SrgTools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SrgTools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SrgTools.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package nl.hardijzer.fw.applysrg;
 import java.util.zip.*;
 import java.io.*;
